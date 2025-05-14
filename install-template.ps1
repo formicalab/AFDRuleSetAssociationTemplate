c7b14f1428eb72385079e728bc54e7a8507db775
Marcello Formica <mformica@microsoft.com>
2025-05-14 15:26:00 +0200

    small updates to install-template.ps1

--- install-template.ps1
+++ install-template.ps1
@@ -1,9 +1,19 @@
-New-AzTemplateSpec -ResourceGroupName "rg-templates-itn-001" `
-                   -Name "ts-afd-itn-001" `
-                   -DisplayName "FrontDoor RuleSet" `
-                   -Description "Associate RuleSet to AFD Rules" `
-                   -Version "1.0.3" `
-                   -Location italynorth `
+#Requires -PSEdition Core
+using module Az.Resources
+
+$templateSpecRG = "rg-templates-itn-001"
+$templateSpecName = "ts-afd-itn-001"
+$templateSpecVersion = "1.1.0"
+$templateSpecLocation = "italynorth"
+$templateSpecDisplayName = "FrontDoor RuleSet"
+$templateSpecDescription = "Associate RuleSet to AFD Rules"
+
+New-AzTemplateSpec -ResourceGroupName $templateSpecRG `
+                   -Name $templateSpecName `
+                   -DisplayName $templateSpecDisplayName `
+                   -Description $templateSpecDescription `
+                   -Version $templateSpecVersion `
+                   -Location $templateSpecLocation `
                    -TemplateFile .\template.json `
                    -UIFormDefinitionFile (Resolve-Path -Path ".\uiFormDefinition.json").Path `
                    -Verbose
