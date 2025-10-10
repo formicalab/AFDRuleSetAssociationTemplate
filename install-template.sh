f8a2bf585eda43f3ef9f50c79617652513a17ed1
Marcello Formica <mformica@microsoft.com>
2025-10-10 15:54:19 +0200

    Fix ui-form-definition-file option in script

---
+++ install-template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+templateSpecRG="rg-templates-itn-001"
+templateSpecName="ts-afd-itn-001"
+templateSpecVersion="1.1.0"
+templateSpecLocation="italynorth"
+templateSpecDisplayName="FrontDoor RuleSet"
+templateSpecDescription="Associate RuleSet to AFD Rules"
+
+az ts create \
+  --resource-group "$templateSpecRG" \
+  --name "$templateSpecName" \
+  --version "$templateSpecVersion" \
+  --location "$templateSpecLocation" \
+  --display-name "$templateSpecDisplayName" \
+  --description "$templateSpecDescription" \
+  --template-file ./template.json \
+  --ui-form-definition ./uiFormDefinition.json \
+  --verbose
